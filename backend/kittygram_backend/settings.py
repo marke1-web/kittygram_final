7b604acde13f223e0dd9f231f8e994ea4336c1e0
Егор Бойко <ego.boiko2018@yandex.ru>
2023-07-25 20:45:34 +0300

    new5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "a")
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
